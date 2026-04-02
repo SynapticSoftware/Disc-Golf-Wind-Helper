d50ec8439a6519e77767aba64e34377b6380efff
Leif Nervick <135361039+lcnervick@users.noreply.github.com>
2026-04-02 18:00:09 -0500

    fix colors in results

--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ['./App.tsx', './src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './App.tsx',
+    './src/**/*.{js,jsx,ts,tsx}',
+    '../../packages/core/src/**/*.{js,jsx,ts,tsx}',
+  ],
   presets: [require('nativewind/preset')],
   theme: { extend: {} },
   plugins: [],
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,5 +1,9 @@
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+    '../../packages/core/src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: { extend: {} },
   plugins: [],
 };
